d6a552a7bc35824bad9c2980c7c3827e56ffae9c
ajay-acer <73325359+ajay-acer@users.noreply.github.com>
2021-05-16 12:02:18 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -22,10 +22,16 @@ def transfer():
     form_data=request.form
     fr=form_data['from']
     amt=form_data['amt']
+    if float(amt)<0:
+        return render_template('incrt.html')
     t=form_data['to']
     print(form_data)
     with sqlite3.connect("BankDB.db") as conn:
         c = conn.cursor()
+        c.execute('''SELECT COUNT(*) FROM BANK WHERE Name=?''',(fr,))
+        r=c.fetchone()
+        if r[0]==0:
+            return render_template('incrt.html')
         c.execute('''UPDATE BANK SET Balance=Balance-? where Name=? and Balance>=?''',(amt,fr,amt))
         if conn.total_changes==0:
             return render_template('failed.html')
